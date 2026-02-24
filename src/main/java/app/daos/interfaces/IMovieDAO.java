a876b5420dfb6024db301d09e7530117315f5e56
Jesper TA <Axicon89@gmail.com>
2026-02-24 12:40:53 +0100

    added needed methods for interface

--- src/main/java/app/daos/interfaces/IMovieDAO.java
+++ src/main/java/app/daos/interfaces/IMovieDAO.java
@@ -1,5 +1,15 @@
 package app.daos.interfaces;
 
+import app.entities.Movie;
+
+import java.util.List;
+
 public interface IMovieDAO
 {
+    List<Movie> getByGenre(String genre);
+    List<Movie> getByTitle(String query);
+    List<Movie> getAverageRating();
+    List<Movie> getTopRated(int limit);
+    List<Movie> getLowestRated(int limit);
+    List<Movie> getMostPopular(int limit);
 }
