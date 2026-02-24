d3c65d9d9eafdb03e4b9d5c9338ac5a7c59febea
Jesper TA <Axicon89@gmail.com>
2026-02-24 10:55:05 +0100

    added entities to EntityRegistry

--- src/main/java/app/config/EntityRegistry.java
+++ src/main/java/app/config/EntityRegistry.java
@@ -13,6 +13,10 @@ final class EntityRegistry
     static void registerEntities(Configuration configuration)
     {
         // TODO: Add entities here...
+        configuration.addAnnotatedClass(Movie.class);
+        configuration.addAnnotatedClass(Actor.class);
+        configuration.addAnnotatedClass(Genre.class);
+        configuration.addAnnotatedClass(Director.class);
         //configuration.addAnnotatedClass(User.class);
     }
 }
